a7340895e752f52e0b6f4b73ba6418922deb8a11
ReelViewsSmartBot <241178641+msprivateliimited-jpg@users.noreply.github.com>
2025-11-02 17:35:01 +0530

    Update bot_backend.py

--- bot_backend.py
+++ bot_backend.py
@@ -20,7 +20,7 @@ creds = ServiceAccountCredentials.from_json_keyfile_name("service_account.json",
 client = gspread.authorize(creds)
 sheet1 = client.open_by_url("https://docs.google.com/spreadsheets/d/1RUOzf-1dM7C87CJtXo2Ae919sx_BSMDfu-APHKytFyU/edit?usp=drivesdk").worksheet("Sheet1")
 sheet2 = client.open_by_url("https://docs.google.com/spreadsheets/d/1RUOzf-1dM7C87CJtXo2Ae919sx_BSMDfu-APHKytFyU/edit?usp=drivesdk").worksheet("Sheet1")
-sheet3 = client.open_by_url("https://docs.google.com/spreadsheets/d/1RUOzf-1dM7C87CJtXo2Ae919sx_BSMDfu-APHKytFyU/edit?usp=drivesdk").worksheet("Sheet1")
+sheet3 = client.open_by_url("https://docs.google.com/spreadsheets/d/1RUOzf-1dM7C87CJtXo2Ae919sx_BSMDfu-APHKytFyU/edit?usp=drivesdk").worksheet("Sheet3")
 
 # === FUNCTION TO REMOVE DUPLICATES ===
 def remove_duplicates(links):
@@ -76,4 +76,5 @@ def count_views(message):
 # === RUN BOT ===
 
 bot.polling(none_stop=True)
+
 
